1c8ee057575e9f33fa49d1a1b3792777c9f31ecc
eideard-hm <ehhernandez81@misena.edu.co>
2022-05-03 11:53:16 -0500

    Test in react components with jest

--- react-counter-app-0.4.0/src/index.js
+++ react-counter-app-0.4.0/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import CounterApp from './CounterApp';
+import PrimeraApp from './PrimeraApp';
+// import CounterApp from './CounterApp';
 
 import './index.css';
 
 const root = createRoot(document.querySelector('#app'));
-root.render(<CounterApp value={10} />)
+// root.render(<CounterApp value={10} />)
+root.render(<PrimeraApp saludo='Hola, soy Goku' />)
